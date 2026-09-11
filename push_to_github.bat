c58253a5eb5239689939112dff940c3c8d3744ca
HARSHAL <kandekarharshal21@gmail.com>
2026-09-11 14:10:13 +0300

    chore: add push_to_github helper script

---
+++ push_to_github.bat
@@ -0,0 +1,9 @@
+@echo off
+echo =======================================================
+echo   Pushing KSHETRA to GitHub Repository
+echo   https://github.com/kandekarharshal21-netizen/KH058-Neurohack.git
+echo =======================================================
+echo.
+git push -u origin main
+echo.
+pause
